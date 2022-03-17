be34dd9a6b983885af2e0df4f7e318078a858f42
zxcv13579 <j0910290373@gmail.com>
2022-03-17 11:39:49 +0800

    template2 新增 reverse effect

--- src/common/EntryAnimate.js
+++ src/common/EntryAnimate.js
@@ -8,7 +8,6 @@ export default class EntryAnimate {
     this.windowWidth = window.parent.innerWidth;
     this.distance;
     this.reverse = reverse;
-    // 0316
     this.chileNodes = document.querySelectorAll('.entryAnimate__container > *');
     this.effect = effect;
 
@@ -18,23 +17,8 @@ export default class EntryAnimate {
     this.init();
   }
   init() {
-    // this.#target = document.querySelector('.entryAnimate__target');
-    // if (!this.#target) {
-    //   throw new Error("doesn't find [entryAnimate__target]");
-    // }
-    // this.distance = this.windowHeight / 2 + this.#target.offsetHeight / 2;
-
-    // gsap.set(this.#target, {
-    //   y: `${!this.reverse ? '-' : ''}${this.distance}`,
-    // });
-
-    // 0316
     const newEl = Array.prototype.slice.call(this.chileNodes).map((i, k) => {
       const items = document.createElement('div');
-      // items.classList.add(
-      //   this.template === 'template1' ? 'entryAnimate__items' : 'entryAnimate__items',
-      //   'entryAnimate__items--multiple',
-      // );
       items.classList.add('entryAnimate__items');
       const cloneEl = i.cloneNode(true);
       items.append(cloneEl);
@@ -50,15 +34,12 @@ export default class EntryAnimate {
     } else {
       this.distance = this.windowWidth / 2 + referEl.offsetWidth / 2;
       document.querySelectorAll('.entryAnimate__items > *').forEach((i, k) => {
-        // console.log(`${k % 2 === 1 && '-'}${this.distance}`);
-        gsap.set(i, { x: `${k % 2 === 0 ? '-' : ''}${this.distance}`, opacity: 1 });
+        gsap.set(i, {
+          x: `${k % 2 === 0 ? (!this.reverse ? '-' : '') : !this.reverse ? '' : '-'}${this.distance}`,
+          opacity: 1,
+        });
       });
     }
-    // this.start();
-    // for (let i = 0; i < document.querySelectorAll('.entryAnimate__items').length; i++) {
-    //   const el = document.querySelectorAll('.entryAnimate__items')[i];
-    //   gsap.set(el, { y: `${!this.reverse ? '-' : ''}${this.distance}` });
-    // }
   }
   start() {
     // lock window scroll
@@ -77,37 +58,10 @@ export default class EntryAnimate {
       default:
         break;
     }
-
-    // animate
-    // if does't set delay, animate will start with onFullScreen effect together
-    // const tl = gsap.timeline({ delay: 0.5 });
-    // tl.to(this.#target, {
-    //   y: 0,
-    //   opacity: 1,
-    //   duration: 0.8,
-    //   ease: 'easeOut',
-    // });
-    // .to(
-    //   this.#target,
-    //   {
-    //     y: `${this.reverse ? '-' : ''}${this.distance}`,
-    //     opacity: 0,
-    //     duration: 0.8,
-    //     ease: 'easeIn',
-    //     onComplete: () => {
-    //       // entry animate end
-    //       document.querySelector('.entryAnimate').remove();
-    //     },
-    //   },
-    //   '+=1.7',
-    // );
   }
   template1() {
     this.tl = gsap.timeline({ delay: 0.5 });
     const target = document.querySelector('.entryAnimate__items > img');
-    // gsap.set(target, {
-    //   y: `${!this.reverse ? '-' : ''}${this.distance}`,
-    // });
     this.tl
       .fromTo(
         target,
@@ -141,7 +95,6 @@ export default class EntryAnimate {
       document.querySelector('.entryAnimate__container').offsetTop +
       document.querySelector('.entryAnimate__container').offsetHeight;
     this.tl = gsap.timeline({ delay: 0 });
-    document.querySelectorAll('.entryAnimate__items > *').forEach((i, k) => {});
     document.querySelectorAll('.entryAnimate__items > *').forEach((i, k) => {
       gsap.to(i, { x: 0, ease: 'easeOut', duration: 0.8, delay: `${k === 2 ? '0.2 ' : '0'}` });
     });
