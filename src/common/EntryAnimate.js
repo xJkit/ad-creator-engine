b8b3e48af3c7e09fa2ce5b428ead4aeb7d165bdb
zxcv13579 <j0910290373@gmail.com>
2022-03-17 15:39:09 +0800

    create template3

--- src/common/EntryAnimate.js
+++ src/common/EntryAnimate.js
@@ -29,16 +29,32 @@ export default class EntryAnimate {
     document.querySelector('.entryAnimate__container').append(...newEl);
 
     const referEl = document.querySelector('.entryAnimate__items').firstChild;
-    if (this.effect === 'template1') {
-      this.distance = this.windowHeight / 2 + referEl.offsetHeight / 2;
-    } else {
-      this.distance = this.windowWidth / 2 + referEl.offsetWidth / 2;
-      document.querySelectorAll('.entryAnimate__items > *').forEach((i, k) => {
-        gsap.set(i, {
-          x: `${k % 2 === 0 ? (!this.reverse ? '-' : '') : !this.reverse ? '' : '-'}${this.distance}`,
-          opacity: 1,
+    // if (this.effect === 'template1') {
+    //   this.distance = this.windowHeight / 2 + referEl.offsetHeight / 2;
+    // } else {
+    //   this.distance = this.windowWidth / 2 + referEl.offsetWidth / 2;
+    //   document.querySelectorAll('.entryAnimate__items > *').forEach((i, k) => {
+    //     gsap.set(i, {
+    //       x: `${k % 2 === 0 ? (!this.reverse ? '-' : '') : !this.reverse ? '' : '-'}${this.distance}`,
+    //       opacity: 1,
+    //     });
+    //   });
+    // }
+    switch (this.effect) {
+      case 'template1':
+        this.distance = this.windowHeight / 2 + referEl.offsetHeight / 2;
+        break;
+      case 'template2':
+        this.distance = this.windowWidth / 2 + referEl.offsetWidth / 2;
+        document.querySelectorAll('.entryAnimate__items > *').forEach((i, k) => {
+          gsap.set(i, {
+            x: `${k % 2 === 0 ? (!this.reverse ? '-' : '') : !this.reverse ? '' : '-'}${this.distance}`,
+            opacity: 1,
+          });
         });
-      });
+        break;
+      default:
+        break;
     }
   }
   start() {
@@ -48,16 +64,7 @@ export default class EntryAnimate {
     window.parent.document.body.style.overflow = 'hidden';
     window.parent.document.body.style.height = '100vh';
 
-    switch (this.effect) {
-      case 'template1':
-        this.template1();
-        break;
-      case 'template2':
-        this.template2();
-        break;
-      default:
-        break;
-    }
+    this[this.effect]();
   }
   template1() {
     this.tl = gsap.timeline({ delay: 0.5 });
@@ -104,6 +111,31 @@ export default class EntryAnimate {
       ease: 'easeIn',
       duration: 0.5,
       delay: 2.5,
+      onComplete: () => {
+        // entry animate end
+        document.querySelector('.entryAnimate').remove();
+      },
     });
+  }
+  template3() {
+    this.tl = gsap.timeline({ delay: 0.5 });
+    const target = document.querySelector('.entryAnimate__items > img');
+    gsap.set(target, { opacity: 1 });
+    gsap.fromTo(target, { scale: 0 }, { scale: 1, duration: 0.5 });
+    this.tl
+      .to(target, { scale: 1.3, ease: 'easeIn', duration: 0.5 }, '+=0.5')
+      .to(target, { scale: 1, ease: 'easeOut', duration: 0.5 })
+      .to(target, { scale: 1.3, ease: 'easeIn', duration: 0.5 })
+      .to(target, { scale: 1, ease: 'easeOut', duration: 0.5 })
+      .to(target, {
+        scale: 5,
+        opacity: 0,
+        ease: 'easeIn',
+        duration: 0.5,
+        onComplete: () => {
+          // entry animate end
+          document.querySelector('.entryAnimate').remove();
+        },
+      });
   }
 }
